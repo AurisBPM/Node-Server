d378cd4bfd07aaa65d45b1f640b7d4415ba53c76
Aurimas <aurimas.ciuplinskas@gmail.com>
2025-10-21 23:10:32 +0300

    Return

--- Moerie/server/app.js
+++ Moerie/server/app.js
@@ -6,7 +6,16 @@ const qaRoute = require("./src/routes/qa");
 const server = express();
 
 // Configure CORS to allow requests from Zendesk
-server.use(cors());
+server.use(cors({
+  origin: [
+    'https://1167845.apps.zdusercontent.com',
+    'https://1167762.apps.zdusercontent.com',
+    'https://moeriehelp.zendesk.com',
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-requested-with']
+}));
 
 server.use(express.json()); 
 server.use(express.urlencoded({ extended: true })); 
