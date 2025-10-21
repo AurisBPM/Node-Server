2d08a4e3924d0895e18045f69227ba285bbfc573
Aurimas <aurimas.ciuplinskas@gmail.com>
2025-10-21 19:06:13 +0300

    ENv adjustments

--- Moerie/server/src/config/db-config.js
+++ Moerie/server/src/config/db-config.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 const DB_CONFIG = {
-    host: 'localhost',
-    user: 'root',
-    password: 'new_password',
-    database: 'sys',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
   };
   
   module.exports = DB_CONFIG;
